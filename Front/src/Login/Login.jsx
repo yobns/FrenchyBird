9bd9c30e8d493167c1e1e1c34b08ee89e6372819
Yoni Bensoussan <bns@air-de-yoni.home>
2024-02-17 23:51:26 +0100

    v2

--- Front/src/Login/Login.jsx
+++ Front/src/Login/Login.jsx
@@ -3,28 +3,31 @@ import './Login.css'
 import { ModalContext } from '../Context/ModalsContext';
 import { useNavigate } from 'react-router-dom';
 
-const Home = () => {
+const Login = () => {
   const { handleLoginShow, handleSignupShow, isUserLoggedIn } = useContext(ModalContext);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(isUserLoggedIn)
+    if (isUserLoggedIn)
       navigate('/Game');
   }, [isUserLoggedIn, navigate])
-  
+
   return (
-    <div className='home'>
-      {!isUserLoggedIn && (<>
-        <div className="msgCard">
-          <h1 className='msg'>To access <span className="msgLogo">Frenchy<span style={{ color: '#ffea88', fontStyle: 'italic' }}>Bird</span></span> please Login</h1>
-          <div className="btnContainer">
-            <button className='btnLogin' onClick={handleLoginShow}>Login</button>
-            <button className='btnLogin' onClick={handleSignupShow}>Signup</button>
+    <div className='login'>
+      <div className="borderBg">
+        <span className="title">Frenchy<span style={{ color: '#ffea88', fontStyle: 'italic' }}>Bird</span></span>
+        {!isUserLoggedIn && (<>
+          <div className="msgCard">
+            <h1 className='msg'>To access <span className="msgLogo">Frenchy<span style={{ color: '#ffea88', fontStyle: 'italic', marginRight: '13px' }}>Bird</span></span>please login</h1>
+            <div className="btnContainer">
+              <button className='btnLogin' onClick={handleLoginShow}>Login</button>
+              <button className='btnLogin' onClick={handleSignupShow}>Signup</button>
+            </div>
           </div>
-        </div>
-      </>)}
+        </>)}
+      </div>
     </div>
   )
 }
 
-export default Home
+export default Login
--- Front/src/Nav/Navbar.jsx
+++ Front/src/Nav/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import './Navbar.css'
 import { NavLink, useNavigate } from 'react-router-dom';
+import { ModalContext } from '../Context/ModalsContext';
 
 function NavBar() {
+  const { handleLoginShow, handleSignupShow, isUserLoggedIn } = useContext(ModalContext);
   const navigate = useNavigate();
   const [expanded, setExpanded] = useState(false);
 
@@ -17,14 +19,22 @@ function NavBar() {
         <Navbar.Brand href="/Game"><span className="logo">Frenchy<span style={{ color: '#ffea88', fontStyle: 'italic' }}>Bird</span></span></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(!expanded)} />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink to="/Game" className={({ isActive }) => isActive ? 'activeLink' : 'link'} onClick={closeNavOnLinkClick}>Play!</NavLink>
-            <NavLink to="/Rank" className={({ isActive }) => isActive ? 'activeLink' : 'link'} onClick={closeNavOnLinkClick}>Rank</NavLink>
-          </Nav>
-          <Nav>
-            <button className='accountBtn' onClick={() => { navigate('/Profile'); closeNavOnLinkClick(); }}>Profile</button>
-          </Nav>
+          {isUserLoggedIn ? (<>
+            <Nav className="me-auto">
+              <NavLink to="/Game" className={({ isActive }) => isActive ? 'activeLink' : 'link'} onClick={closeNavOnLinkClick}>Play!</NavLink>
+              <NavLink to="/Rank" className={({ isActive }) => isActive ? 'activeLink' : 'link'} onClick={closeNavOnLinkClick}>Rank</NavLink>
+            </Nav>
+            <Nav>
+              <button className='accountBtn' onClick={() => { navigate('/Profile'); closeNavOnLinkClick(); }}>Profile</button>
+            </Nav>
+          </>) : (<>
+            <Nav>
+              <button className='accountBtn' onClick={() => { handleLoginShow() }} style={{marginRight: '30px'}}>Login</button>
+              <button className='accountBtn' onClick={() => { handleSignupShow() }}>Signup</button>
+            </Nav>
+          </>)}
         </Navbar.Collapse>
+
       </Container>
     </Navbar>
   );
